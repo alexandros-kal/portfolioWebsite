27d3b390edb00eedab4537cde54a134fc964a881
alexandros-kal <kalikasalexandros@outlook.com>
2025-01-22 17:10:27 +0200

    Responsive Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,18 +1,49 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom'; 
-import { FaGithub } from 'react-icons/fa';
+import { FaGithub, FaBars, FaTimes } from 'react-icons/fa';
 import './Navbar.css'
 
 const Navbar = () => {
-    return(
+   const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+   const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+   }
+
+   return(
         <nav className='navbar'>
-            <Link to="/">Home</Link> 
-            <Link to="/contact">Contact Me</Link>
-            <Link to="/projects">My projects</Link>
-            <Link to='/Education'>My Education</Link>
-            <a href='https://github.com/alexandros-kal' target='_blank' rel="noreferrer"><FaGithub size={50} /></a>
+            <div className='navbar-container'>
+                <Link to="/" className='logo'>MyPortfolio</Link>
+                <button className='menu-toggle' onClick={toggleMenu}>
+                    {isMenuOpen ? < FaTimes size={30} /> : <FaBars size={30} />}
+                </button>
+
+                <ul className={`nav-links ${isMenuOpen ? 'open' : ''}`}>
+                    <li>
+                        <Link to="/contact" onClick={() => setIsMenuOpen(false)}>Contact Me</Link>
+                    </li>
+
+
+                    <li>
+                        <Link to="/projects" onClick={() => setIsMenuOpen(false)}>My Projects</Link>
+                    </li>
+
+
+                    <li>
+                        <Link to="/education" onClick={() => setIsMenuOpen(false)}>My Education</Link>
+                    </li>
+
+                    <li>
+                        <a href="https://github.com/alexandros-kal" target="_blank" rel="noreferrer">
+                            <FaGithub size={30} />
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div>
         </nav>
-    )
+   )
 }
 
-export default Navbar
+export default Navbar;
